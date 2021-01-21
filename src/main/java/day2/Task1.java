1c9e812d83ce78298a03906312d31730aa489542
Sergey <naskinsergey1975@gmail.com>
2021-01-21 15:46:54 +0700

    complete day2 tasks

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,23 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
 
+        if (i <= 0){
+            System.out.println("Ошибка ввода");
+        }
+        else if (i >= 1 && i <= 4){
+            System.out.println("Малоэтажный дом");
+        }
+        else if (i > 4 && i <= 8){
+            System.out.println("Среднеэтажный дом");
+        }
+        else {
+            System.out.println("Многоэтажный дом");
+        }
     }
 }
--- src/main/java/day2/Task4.java
+++ src/main/java/day2/Task4.java
@@ -1,7 +1,28 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task4 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System. in);
+        double x = scanner.nextDouble();
+
+        if (x >= 5){
+           double y = (x*x - 10) / (x + 7);
+            System.out.println(y);
+        }
+
+        else if (x > -3 && x < 5){
+            double y = (x + 3) * (x*x - 2);
+            System.out.println(y);
+        }
+
+        else {
+            double y = 420;
+            System.out.println(y);
+        }
+
+
 
     }
 }
