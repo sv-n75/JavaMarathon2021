fbc33b8600e4a352956e33bcfc8837a01c6a9aec
Sergey <naskinsergey1975@gmail.com>
2021-02-06 22:51:31 +0700

    complete day15 tasks

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,28 @@
 package day15;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("C:\\Users\\123\\Desktop\\Javanail\\JavaMarathon2021\\src\\main\\resources\\shoes.csv");
+        PrintWriter pw = new PrintWriter("C:\\Users\\123\\Desktop\\Javanail\\JavaMarathon2021\\src\\main\\resources\\missing_shoes");
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String string = scanner.nextLine();
+                String[] strings = string.split(";");
+                int number = Integer.parseInt(strings[strings.length - 1]);
+                if (number == 0) {
+                    pw.println(string);
+                }
+            }
+            scanner.close();
+            pw.close();
+        } catch (FileNotFoundException e) {
+            System.out.println();
+        }
     }
 }
